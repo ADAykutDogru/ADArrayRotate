495f622eaeeb1e666437642df6517092196299bd
Aykut Dogru <aykutdogru@Aykuts-MacBook-Pro.local>
2014-12-15 20:59:22 +0200

    Initial Commit

---
+++ ADArrayRotate/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ADArrayRotate
+//
+//  Created by Aykut Dogru on 15/12/14.
+//  Copyright (c) 2014 Aykut Dogru. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
